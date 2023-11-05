73255d0b709370ef276300e183ddde828aa3bc9d
ib31iat <rtobias286@gmail.com>
2023-11-05 14:50:14 +0100

    Update box class name
    Co-authored-by: fhehli <fhehli@users.noreply.github.com>

--- code/box.py
+++ code/box.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 
-class AbstractBox:
+class Box:
     def __init__(self, lb: torch.Tensor, ub: torch.Tensor):
         assert lb.shape == ub.shape
         assert (lb > ub).sum() == 0
@@ -18,7 +18,7 @@ class AbstractBox:
         return True
 
     @staticmethod
-    def construct_initial_box(x: torch.Tensor, eps: float) -> "AbstractBox":
+    def construct_initial_box(x: torch.Tensor, eps: float) -> "Box":
         x = x.flatten()
         lb = x - eps
         lb.clamp_(min=0, max=1)
@@ -26,4 +26,4 @@ class AbstractBox:
         ub = x + eps
         ub.clamp_(min=0, max=1)
 
-        return AbstractBox(lb, ub)
+        return Box(lb, ub)
