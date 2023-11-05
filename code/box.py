7f3e82b0f4b1aa06e05e88290dfab65f610c7acc
ib31iat <rtobias286@gmail.com>
2023-11-05 14:06:17 +0100

    Update box.py: Add postcondition check

--- code/box.py
+++ code/box.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+
 class AbstractBox:
     def __init__(self, lb: torch.Tensor, ub: torch.Tensor):
         assert lb.shape == ub.shape
@@ -8,9 +9,17 @@ class AbstractBox:
         self.lb = lb
         self.ub = ub
 
+    def check_postcondition(self, y) -> bool:
+        target = y
+        target_lb = self.lb[target].item()
+        for i in range(self.ub.shape[-1]):
+            if i != target and self.ub[i] >= target_lb:
+                return False
+        return True
 
     @staticmethod
-    def construct_initial_box(x: torch.Tensor, eps: float) -> 'AbstractBox':
+    def construct_initial_box(x: torch.Tensor, eps: float) -> "AbstractBox":
+        x = x.flatten()
         lb = x - eps
         lb.clamp_(min=0, max=1)
 
