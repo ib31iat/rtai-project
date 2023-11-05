0bfb7ced66515cfdc23824022842e3661b1373ce
Fabio Hehli <f4biohehli@hotmail.com>
2023-11-05 11:49:32 +0100

    move attach_shapes

--- code/utils/attach_shapes.py
+++ code/utils/attach_shapes.py
@@ -1,7 +1,6 @@
+import numpy as np
 import torch
 import torch.nn as nn
-
-from networks import dln_conv_model, dln_model, fc_model, conv_model
 
 
 def attach_shapes(model, input_size):
@@ -11,13 +10,17 @@ def attach_shapes(model, input_size):
 
     def register_hook(module):
         def hook(module, input, output):
-            input_shape = list(input[0].size())[1:]
+            input_shape = list(input[0].size())
             module.input_shape = input_shape
+            if not hasattr(module, "in_features"):
+                module.in_features = np.prod(input_shape[1:])  # [1:] to skip batch dimension
             if isinstance(output, (list, tuple)):
-                output_shape = [[-1] + list(o.size())[1:] for o in output]
+                output_shape = [[-1] + list(o.size()) for o in output]
             else:
-                output_shape = list(output.size())[1:]
+                output_shape = list(output.size())
             module.output_shape = output_shape
+            if not hasattr(module, "out_features"):
+                module.out_features = np.prod(output_shape[1:])  # [1:] to skip batch dimension
 
         if not isinstance(module, nn.Sequential) and not isinstance(module, nn.ModuleList):
             hooks.append(module.register_forward_hook(hook))
@@ -36,4 +39,4 @@ def attach_shapes(model, input_size):
 # shapes = attach_shapes(net, (1, 3, 32, 32))
 # for module in net.modules():
 #     if type(module) != nn.Sequential:
-#         print(module.input_shape, module.output_shape)
+#         print(module.input_shape, module.output_shape, module.in_features, module.out_features)
